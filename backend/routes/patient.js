0aeddbf0a1a88a1045f7401f78e7c3aea7d55d28
moazyousaf <moazyousaf1010@gmail.com>
2025-09-26 17:46:52 +0200

    Added the first route for fetching the patients list from the dataabse

--- backend/routes/patient.js
+++ backend/routes/patient.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db/config');
+const db = require('../db/config');
+
+//router responses
+
+router.get('/list', async (req, res) => {
+  try {
+    const query = 'SELECT id, name, email FROM patients ORDER BY id';
+    const result = await db.any(query);
+    res.json(result);
+  } catch (err) {
+    console.error('Patient list error: ', err);
+    res.status(500).json({ error: 'Database error fetching patients' });
+  }
+});
 
 module.exports = router;
