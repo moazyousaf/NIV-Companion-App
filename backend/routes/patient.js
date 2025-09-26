20d62e2d29105cc71404f071962e87c4ce4c3fd3
moazyousaf <moazyousaf1010@gmail.com>
2025-09-27 01:09:11 +0200

    Added route for fetching a specific patients all data

--- backend/routes/patient.js
+++ backend/routes/patient.js
@@ -4,6 +4,7 @@ const db = require('../db/config');
 
 //router responses
 
+//List of all patients in the database
 router.get('/list', async (req, res) => {
   try {
     const query = 'SELECT id, name, email FROM patients ORDER BY id';
@@ -12,6 +13,29 @@ router.get('/list', async (req, res) => {
   } catch (err) {
     console.error('Patient list error: ', err);
     res.status(500).json({ error: 'Database error fetching patients' });
+  }
+});
+
+//Data of a specific patient
+router.get('/:id', async (req, res) => {
+  const id = req.params.id;
+
+  if (!id) {
+    res.status(404).send('Patient ID not valid');
+  }
+
+  try {
+    const query = 'SELECT * FROM niv_data WHERE patient_id = $1';
+    const result = await db.any(query, [id]);
+
+    if (!result[0]) return res.status(400).send('Patient ID not found');
+
+    res.json(result);
+  } catch (err) {
+    console.log("Error fetching patient's data", err);
+    res
+      .status(500)
+      .json({ error: "Database error during patient's data fetching" });
   }
 });
 
